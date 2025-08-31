78c2424d1617a5e40c67ba5e06fdf3c110a8eef1
Afnan kp <mhdafnan309@gmail.com>
2025-08-31 11:25:12 +0530

    Add JWT authentication middleware and protect task routes

--- back/Router/taskRouter.js
+++ back/Router/taskRouter.js
@@ -1,11 +1,12 @@
 import express from 'express'
 import { createTask, updateTask, getAllDAta, removeTask } from '../controllers/taskController.js'
+import auth from './Middlwares/auth.js'
 
 const router = express.Router()
 
-router.get('/task', getAllDAta)
-router.post('/task', createTask)
-router.put('/task/:id', updateTask)
-router.delete('/task/:id', removeTask)
+router.get('/task', auth, getAllDAta)
+router.post('/task', auth, createTask)
+router.put('/task/:id', auth, updateTask)
+router.delete('/task/:id', auth, removeTask)
 
 export default router
